c3f8afb9d2a380d902af750a25b7944ca3640d1e
hyeongjunCho <hyeongjun@kaist.ac.kr>
2017-07-21 02:49:59 +0000

    Connect vue and django

--- frontend/config/index.js
+++ frontend/config/index.js
@@ -27,7 +27,22 @@ module.exports = {
     autoOpenBrowser: true,
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    proxyTable: {},
+    proxyTable: {
+      "/api": {
+        target: 'http://localhost:13702/api',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      },
+      "/admin": {
+        target: 'http://localhost:13702/admin',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/admin': ''
+        }
+      }
+    },
     // CSS Sourcemaps off by default because relative paths are "buggy"
     // with this option, according to the CSS-Loader README
     // (https://github.com/webpack/css-loader#sourcemaps)
