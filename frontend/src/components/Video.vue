fde8a886d77e40a17c6e02856d546065e4835690
hyeongjunCho <hyeongjun@kaist.ac.kr>
2017-08-20 13:00:47 +0000

    Make video pages

--- frontend/src/components/Video.vue
+++ frontend/src/components/Video.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="video">
-    <iframe width="560" height="315" :src="link" frameborder="0" allowfullscreen></iframe>
+  <div id="video noto-sans">
+    <p class="video-name">{{ video.fields.name }}</p>
+    <iframe width="800" height="450" :src="link" frameborder="0" allowfullscreen></iframe>
   </div>
 </template>
 
@@ -11,14 +12,25 @@ export default {
     let id = this.$route.params.id
     this.$http.get(`/api/videos/${id}`)
       .then((response) => {
-        this.video = JSON.parse(response.data)
-        this.link = this.video[0].fields.link
+        this.video = JSON.parse(response.data)[0]
+        console.log(this.video)
+        this.link = this.video.fields.link
+        console.log(this.link)
       })
   },
   data () {
     return {
+      video: {},
       link: []
     }
   }
 }
 </script>
+
+<style>
+.video-name {
+  font-size: 50px;
+  font-weight: 700;
+  margin-bottom: 10px;
+}
+</style>
--- frontend/src/components/Videos.vue
+++ frontend/src/components/Videos.vue
@@ -1,9 +1,12 @@
 <template>
-  <div class="videos">
-    Videos
-    <div v-for="video in videos" class="video">
-      <div>
-        <router-link :to="{ name: 'video', params: { id: video.pk }}">더보기</router-link>
+  <div class="videos noto-sans">
+    <p class="videos-title">비디오 목록</p>
+    <div class="video-list">
+      <div v-for="video in videos" class="video">
+        <div>
+          <iframe width="280" height="280" :src="video.fields.link" frameborder="0" allowfullscreen></iframe>
+          <p class="video-name"><router-link :to="{ name: 'video', params: { id: video.pk }}">{{ video.fields.name }}</router-link></p>
+        </div>
       </div>
     </div>
   </div>
@@ -25,3 +28,30 @@ export default {
   }
 }
 </script>
+
+<style>
+.video-name > a {
+  font-size: 32px;
+  color: black;
+}
+
+.videos-title {
+  font-size: 64px;
+  font-weight: 700;
+  margin-bottom: 10px;
+}
+
+.video-list {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.video {
+  width: 290px;
+}
+
+.video > div {
+  margin-right: 10px;
+}
+</style>
