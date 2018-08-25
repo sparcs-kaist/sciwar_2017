27f42ea8fb71325721ce4d28e2f2962edfbcc6a1
hyeongjun <hyeongjun@kaist.ac.kr>
2018-08-25 19:30:34 +0900

    Update settings.py

--- backend/sciwar/settings.py
+++ backend/sciwar/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'port': '3001',
+        'PORT': '3001',
     }
 }
 
